6bf5d81a8893a57ef805d4193edb9b2795782d81
TOVTC <veronica.tc.to@outlook.com>
2023-04-17 13:57:59 -0400

    added example data and for loop in movie list component

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,22 +1,35 @@
 <script setup>
+// import ref to create state
+import { ref } from 'vue'
+
+const movies = ref([
+    {
+        title: "Title 5",
+        year: 2025
+    },
+    {
+        title: "Title 4",
+        year: 2024
+    },
+    {
+        title: "Title 3",
+        year: 2023
+    },
+    {
+        title: "Title 2",
+        year: 2022
+    },
+    {
+        title: "Title 1",
+        year: 2021
+    }
+])
 </script>
 
 <template>
     <ul>
-        <RouterLink :to="{ name: 'movie-details' }">
-            <li>Movie Title (2025)</li>
-        </RouterLink>
-        <RouterLink :to="{ name: 'movie-details' }">
-            <li>Movie Title (2024)</li>
-        </RouterLink>
-        <RouterLink :to="{ name: 'movie-details' }">
-            <li>Movie Title (2023)</li>
-        </RouterLink>
-        <RouterLink :to="{ name: 'movie-details' }">
-            <li>Movie Title (2022)</li>
-        </RouterLink>
-        <RouterLink :to="{ name: 'movie-details' }">
-            <li>Movie Title (2021)</li>
+        <RouterLink :to="{ name: 'movie-details' }" v-for="(movie, index) in movies" :key="index">
+            <li>{{ movie.title }} ({{ movie.year }})</li>
         </RouterLink>
     </ul>
 </template>
