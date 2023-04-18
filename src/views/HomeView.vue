968385f98da8a2ea027420c0f50d6d76bc3f6489
TOVTC <veronica.tc.to@outlook.com>
2023-04-18 10:06:48 -0400

    used .push() method to update router redirect on search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,24 @@
 <script setup>
   import { ref } from 'vue'
-  const movieName = ref(" ")
+  import router from '../router'
+  const movieName = ref(null)
+
+  const search = () => {
+    if (!movieName.value) {
+      alert("please enter a search term")
+      return
+    }
+    router.push({ name: 'results', params: { movieName: movieName.value } })
+  }
 </script>
 
 <template>
   <main>
-    <form id="search-form">
+    <form id="search-form" @submit.prevent="search">
         <label for="movie-name">Enter a movie name:</label><br/>
         <input type="text" id="search" v-model="movieName">
-        <RouterLink :to="{ name: 'results', params: { movieName: movieName } }"><button type="submit">Search</button></RouterLink>
+        <!-- <RouterLink :to="{ name: 'results', params: { movieName: movieName } }"><button type="submit">Search</button></RouterLink> -->
+        <button type="submit">Search</button>
     </form>
   </main>
 </template>
